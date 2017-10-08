32ae846a20d72c644ceb368b8330838797e32061
Dominique Dumont <dod@debian.org>
2017-10-08 19:31:28 +0200

    reformat by cme meta

--- lib/Config/Model/models/SystemSsh.pl
+++ lib/Config/Model/models/SystemSsh.pl
@@ -1,25 +1,27 @@
 [
   {
-    'class_description' => 'Configuration class used by L<Config::Model> to edit or 
-validate /etc/ssh/ssh_config (as root)
-',
-    'name' => 'SystemSsh',
-    'include' => [
-      'Ssh'
-    ],
-    'copyright' => [
-      '2013 Dominique Dumont'
-    ],
     'author' => [
       'Dominique Dumont'
     ],
+    'class_description' => 'Configuration class used by L<Config::Model> to edit or 
+validate /etc/ssh/ssh_config (as root)
+',
+    'copyright' => [
+      '2013 Dominique Dumont'
+    ],
+    'include' => [
+      'Ssh'
+    ],
     'license' => 'LGPL2',
+    'name' => 'SystemSsh',
     'read_config' => [
       {
         'backend' => 'OpenSsh::Ssh',
         'config_dir' => '/etc/ssh',
         'file' => 'ssh_config',
-	'os_config_dir' => { 'darwin' => '/etc' },
+        'os_config_dir' => {
+          'darwin' => '/etc'
+        }
       }
     ]
   }
