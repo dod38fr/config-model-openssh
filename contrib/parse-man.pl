5ac53446906ca6ae0cf8e38d2326577d5c87c75b
Dominique Dumont <dod@debian.org>
2019-03-19 20:43:45 +0100

    add sshd model generation

--- contrib/parse-man.pl
+++ contrib/parse-man.pl
@@ -50,6 +50,36 @@ sub create_ssh_model ($meta_root) {
     $meta_root->load(qq!class:Ssh include="Ssh::HostElement"!);
 }
 
+sub create_sshd_model ($meta_root) {
+    say "Processing sshd documentation...";
+
+    create_class_boilerplate ($meta_root, sshd_system => 'Sshd');
+    create_class_boilerplate ($meta_root, sshd_system => 'Sshd::MatchElement');
+
+    my $data = parse_man_page( 'sshd_config' ) ;
+
+    # retrieve list of keywords that can fit in Match block
+    my $is_match = extract_list_from_desc($data->{element_data}{'Match'});
+
+    foreach my $element ($data->{element_list}->@*) {
+        my @desc = $data->{element_data}{$element}->@*;
+        my $load_string = create_load_data(sshd => $element, @desc);
+        my $target = $is_match->{$element} ? 'Sshd::MatchElement' : 'Sshd';
+        my $obj = $meta_root->grab(qq!class:$target element:"$element"!);
+        $obj->load($load_string);
+        $obj->fetch_element("description")->store(join('', @desc));
+    }
+
+    $meta_root->load(qq!class:Sshd include="Sshd::MatchElement"!);
+}
+
+sub extract_list_from_desc ($desc_ref) {
+    my $str = $desc_ref->[$#$desc_ref];
+    my @keywords = ( $str =~ /B<(\w+)>/g );
+    my %is_match = map { $_ => 1 ; } grep { $_ ne 'Match' } @keywords;
+    return \%is_match;
+}
+
 sub load_yaml_model ($meta_root,$class) {
     my $file = 'contrib/'.lc($class).'.yml';
     $file =~ s/::/-/g;
@@ -77,7 +107,18 @@ $meta_root->load_data(LoadFile('contrib/ssh-fixup.yaml'));
 # created
 load_yaml_model($meta_root,"SystemSsh");
 
+say "Ssh model is done...";
+
+say "Generating Sshd model";
+
+load_yaml_model($meta_root,"Sshd::MatchCondition");
+
+create_sshd_model($meta_root);
+
+# requires Sshd::MatchElement
+#load_yaml_model($meta_root,"Sshd::MatchBlock");
+
 say "Saving ssh model...";
-$rw_obj->write_all;
+# $rw_obj->write_all;
 
 say "Done.";
