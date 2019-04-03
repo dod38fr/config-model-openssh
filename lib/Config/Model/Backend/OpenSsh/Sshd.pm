1a7699c637efc937a4220733b4b2f66dbc97c8b7
Dominique Dumont <dod@debian.org>
2019-04-03 18:53:29 +0200

    created new role to read/write Match block
    This prepare the backend for newer version of OpenSsh where Match block
    are also possible in Ssh config (and not only in Sshd)

--- lib/Config/Model/Backend/OpenSsh/Sshd.pm
+++ lib/Config/Model/Backend/OpenSsh/Sshd.pm
@@ -3,6 +3,8 @@ package Config::Model::Backend::OpenSsh::Sshd ;
 use Mouse ;
 extends "Config::Model::Backend::OpenSsh" ;
 
+with 'Config::Model::Backend::OpenSsh::Role::MatchBlock';
+
 use Carp ;
 use IO::File ;
 use Log::Log4perl;
@@ -10,29 +12,6 @@ use File::Copy ;
 use File::Path ;
 
 my $logger = Log::Log4perl::get_logger("Backend::OpenSsh");
-
-sub match {
-    my ($self, $root, $key, $pairs, $comment, $check) = @_ ;
-    $logger->debug("match: @$pairs # $comment");
-    my $list_obj = $root->fetch_element('Match');
-
-    # create new match block
-    my $nb_of_elt = $list_obj->fetch_size;
-    my $block_obj = $list_obj->fetch_with_id($nb_of_elt) ;
-    $block_obj->annotation($comment) ;
-
-    while (@$pairs) {
-        my $criteria = shift @$pairs;
-        my $pattern  = shift @$pairs;
-        $block_obj->load(
-            steps => qq!Condition $criteria="$pattern"!,
-            check => $check,
-        );
-    }
-
-    $self->current_node( $block_obj->fetch_element('Settings') );
-}
-
 
 # now the write part
 sub write {
@@ -44,62 +23,6 @@ sub _write_line {
     return sprintf("%-20s %s\n",@_) ;
 }
 
-sub write_all_match_block {
-    my $self = shift ;
-    my $match_elt = shift ;
-    my $mode = shift || '';
-
-    my $result = '';
-    foreach my $elt ($match_elt->fetch_all($mode) ) {
-        $result .= $self->write_match_block($elt,$mode) ;
-    }
-
-    return $result ;
-}
-
-sub write_match_block {
-    my $self = shift ;
-    my $match_elt = shift ;
-    my $mode = shift || '';
-
-    my $match_line ;
-    my $match_body ;
-
-    foreach my $name ($match_elt->get_element_name() ) {
-        my $elt = $match_elt->fetch_element($name) ;
-
-        if ($name eq 'Settings') {
-            $match_body .= $self->write_node_content($elt,$mode)."\n" ;
-        }
-        elsif ($name eq 'Condition') {
-            $match_line = $self->write_line(
-                Match => $self->write_match_condition($elt,$mode) ,
-                $match_elt -> annotation
-            ) ;
-        }
-        else {
-            die "write_match_block: unexpected element: $name";
-        }
-    }
-
-    return $match_line.$match_body ;
-}
-
-sub write_match_condition {
-    my $self = shift ;
-    my $cond_elt = shift ;
-    my $mode = shift || '';
-
-    my $result = '' ;
-
-    foreach my $name ($cond_elt->get_element_name() ) {
-        my $elt = $cond_elt->fetch_element($name) ;
-        my $v = $elt->fetch($mode) ;
-        $result .= " $name $v" if defined $v;
-    }
-
-    return $result ;
-}
 
 no Mouse;
 
